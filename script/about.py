77157e4db277ea0ecbb1a4621c6aa573fd140784
Haitao Zuo <haitaozuo@mac.local>
2019-09-27 07:25:22 +0800

    fix rss_path

--- script/about.py
+++ script/about.py
@@ -7,7 +7,9 @@ class me:
     def GET(self):
         templates = os.path.join(os.path.dirname(__file__), 'templates')
         render = web.template.render(templates)
-        rss = None
-        with open(os.path.join(temp, 'rss'), 'rb') as f:
-            rss = pickle.load(f)
+        rss = []
+        rss_path = os.path.join(temp, 'rss')
+        if os.path.isfile(rss_path):
+            with open(os.path.join(temp, 'rss'), 'rb') as f:
+                rss = pickle.load(f)
         return render.aboutme(rss)
